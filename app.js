fd1ce17eb89cfaf6121a54ca5db69f47555df292
Eunice Otieno <otieunice@gmail.com>
2022-01-18 15:37:45 +0300

    Rendered Html files with ejs template engines

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const express = require('express');
 
 // Initialize express
 const app = express();
+// Set up a view engine
+app.set('view engine', 'ejs');
 
-// Set a static folder
-app.use(express.static('public'));
+app.get('/', (req,res)=>{
+    res.render('index');
+ })
 
 // Define the port number
 const PORT = 5000;
